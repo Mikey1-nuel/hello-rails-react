0cc9c33b1b4dc489d970c6ba2d718a7461e76504
Emmanuel <emmanuelnwoye12@gmail.com>
2023-08-22 15:11:55 +0100

    Add route for messages using api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :messages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    resources :messages, only: [:index]
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homes#index"
 end
